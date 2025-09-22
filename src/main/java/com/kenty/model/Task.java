85863dfc9ff72a88e2a5142f724d622cfbd77228
Kent <khieonqt@gmail.com>
2025-09-22 21:55:15 +0800

    Add: Add task class and taskstatus enum

---
+++ src/main/java/com/kenty/model/Task.java
@@ -0,0 +1,68 @@
+package com.kenty.model;
+
+import java.time.LocalDate;
+
+public class Task implements Comparable<Task>{
+    private final long id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private TaskStatus status;
+
+    private static long taskInstanceCount = 0;
+
+    public Task() {
+        this.id = ++taskInstanceCount;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setStatus(String status) {
+        switch (status) {
+            case "PENDING" -> this.status = TaskStatus.PENDING;
+            case "IN_PROGRESS" -> this.status = TaskStatus.IN_PROGRESS;
+            case "DONE" -> this.status = TaskStatus.DONE;
+        }
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public int compareTo(Task o) {
+        if (this.dueDate.isBefore(o.getDueDate())) {
+            return -1;
+        } else if (this.dueDate.isAfter(o.getDueDate())) {
+            return 1;
+        } else {
+            return this.title.compareTo(o.getTitle());
+        }
+    }
+}
---
+++ src/main/java/com/kenty/model/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.kenty.model;
+
+public enum TaskStatus {
+    PENDING,
+    IN_PROGRESS,
+    DONE
+}
